21d5d86105634c237a900c9d563d6d2e4d302e50
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-02 17:57:14 -0700

    add button with event listener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("main").appendChild(h2);
+
+let count = 1;
+
+function addToCount() {
+    console.log(`You have clicked me ${count} ${count === 1 ? "time" : "times"}!`);
+    count++;
+}
+
+const button = document.getElementsByTagName('button')[0];
+button.textContent = "Click Me!"
+button.addEventListener("click", addToCount);
